5f0c37dc027ca8e0aa68c769eeec44e1456d4718
Jaime Lopez Viartola <jaime.viartola@gmail.com>
2017-01-28 18:02:39 +0100

    Base de datos final
    Signed-off-by: Jaime Lopez Viartola <jaime.viartola@gmail.com>

---
+++ user.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-01-2017 a las 18:01:47
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `jobraider`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `dni` varchar(9) DEFAULT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `nombre`, `dni`, `password`) VALUES
+(1, 'edu', 'edu', '72996405'),
+(2, 'e', 'e', 'e'),
+(3, 'eedu', '72296405', 'edu'),
+(4, 'eedu', '72296405', 'edu'),
+(5, 'jose', 'ausente', '2017-01-05'),
+(6, 'je', '9585', 'je'),
+(7, 'p', NULL, 'p'),
+(8, 'p', NULL, 'p'),
+(9, 'j', NULL, 'j'),
+(10, 'j', NULL, 'j'),
+(11, 'j', NULL, 'j'),
+(12, 'j', NULL, 'j'),
+(13, 'j', NULL, 'j'),
+(14, 'jaime', '1234567G', '12'),
+(15, 'Name', NULL, 'Name'),
+(16, 'Jaime', '123456789', '1234'),
+(17, 'p', '123456789', 'p'),
+(18, 'a', 'a', 'a');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
